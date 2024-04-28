0197919aa4a3f31c90c4518bb494adfd1097a7fb
zhiavolnov <152493447+zhiavolnov@users.noreply.github.com>
2024-04-28 13:17:47 +0000

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+    main()
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+        print(f"Random Number: {item}")
+    return data
+    for item in data:
+if __name__ == "__main__":
+    data = generate_random_data()
+
+
+
+def main():
